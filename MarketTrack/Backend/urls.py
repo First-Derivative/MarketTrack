e9256bc503f2f5864f4d44f7a4b9b6b1ceb9fe28
First-Derivative <ashraff.hatz@gmail.com>
2021-05-01 20:09:47 +0100

    update url files

--- MarketTrack/Backend/urls.py
+++ MarketTrack/Backend/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-  path('/api/search/<str:query>', search_api),    
-  # path('/<str:user>/collections', collections_view, name="user_collections"),
-  # path('/<str:user>/tracked', tracked_view, name="user_tracked")
-
+  path('search/<str:query>', search_api),
 ]
--- MarketTrack/MarketTrack/urls.py
+++ MarketTrack/MarketTrack/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import include ,path
 
 urlpatterns = [
-path("admin/", admin.site.urls),
-path("", include("Frontend.urls")),
-# path("api/,include("Backend.urls"))
+  path("admin/", admin.site.urls),
+
+  path("", include("Frontend.urls")),
+
+  path("api/",include("Backend.urls")),
+
+  path("user/", include("UserAccounts.urls"))
 ]
